138eb6f84658d3906fe82b4fef6c07ebf96e5db1
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-04-12 15:13:48 +0200

    job details page formated

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import image1 from '../assets/All Images/Vector.png'
-import image2 from '../assets/All Images/Vector-1.png'
+import image1 from "../assets/All Images/Vector.png";
+import image2 from "../assets/All Images/Vector-1.png";
 
 const JobDetails = () => {
   const { jobCardId } = useParams();
@@ -19,27 +19,43 @@ const JobDetails = () => {
     setDetails(jobDetails || {});
   }, [jobCardId, dataDetails]);
 
-   const handleApplyNow = () => {
-    const { id, companyName, jobTitle,jobLocation, img, salary, jobType } = details;
-    const appliedJob = { id, companyName, jobTitle,jobLocation, img, salary, jobType };
+  const handleApplyNow = () => {
+    const { id, companyName, jobTitle, jobLocation, img, salary, jobType } =
+      details;
+    const appliedJob = {
+      id,
+      companyName,
+      jobTitle,
+      jobLocation,
+      img,
+      salary,
+      jobType,
+    };
     if (appliedJobs.some((job) => job.id === id)) {
       toast.error("You have already applied for this job");
     } else {
       setAppliedJobs([...appliedJobs, appliedJob]);
       const savedJobs = JSON.parse(localStorage.getItem("savedJobs")) || [];
-      localStorage.setItem("savedJobs", JSON.stringify([...savedJobs, appliedJob]));
+      localStorage.setItem(
+        "savedJobs",
+        JSON.stringify([...savedJobs, appliedJob])
+      );
       toast.success("You have just applied for this job");
     }
   };
 
   return (
     <div>
-       <div  className="banner mb-40 ">
-       <div className="flex justify-center items-center">
+      <div className="banner mb-40 ">
+        <div className="flex justify-center items-center">
           <h2 className="text-5xl font-bold">Applied Jobs</h2>
         </div>
-        <img  className="absolute top-0 right-0 transform scale-x-1" src={image2} alt="banner" />
-        <img className="absolute top-24"  src={image1} alt="banner" /> 
+        <img
+          className="absolute top-0 right-0 transform scale-x-1"
+          src={image2}
+          alt="banner"
+        />
+        <img className="absolute top-24" src={image1} alt="banner" />
       </div>
       <div className="flex justify-between items-center gap-10  mt-12">
         <div className="w-2/4 border rounded-lg m-auto p-8  flex flex-col gap-6">
@@ -92,9 +108,17 @@ const JobDetails = () => {
               </p>
               <p className="flex gap-2">
                 {" "}
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="18px"
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18px"
                     height="18"
-                    fill="gray" viewBox="0 0 448 512"><path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192h80v56H48V192zm0 104h80v64H48V296zm128 0h96v64H176V296zm144 0h80v64H320V296zm80-48H320V192h80v56zm0 160v40c0 8.8-7.2 16-16 16H320V408h80zm-128 0v56H176V408h96zm-144 0v56H64c-8.8 0-16-7.2-16-16V408h80zM272 248H176V192h96v56z"/></svg></span>
+                    fill="gray"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192h80v56H48V192zm0 104h80v64H48V296zm128 0h96v64H176V296zm144 0h80v64H320V296zm80-48H320V192h80v56zm0 160v40c0 8.8-7.2 16-16 16H320V408h80zm-128 0v56H176V408h96zm-144 0v56H64c-8.8 0-16-7.2-16-16V408h80zM272 248H176V192h96v56z" />
+                  </svg>
+                </span>
                 <span className="font-semibold"> JobTitle:</span>{" "}
                 <span className="text-[#757575]">{details.jobTitle}</span>
               </p>
@@ -106,17 +130,33 @@ const JobDetails = () => {
             <div className="flex flex-col gap-2 mb-4">
               <p className="flex items-center">
                 {" "}
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="18px"
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18px"
                     height="18"
-                    fill="gray"  viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg></span>
+                    fill="gray"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                  </svg>
+                </span>
                 <span className="font-semibold">&nbsp;Phone: &nbsp;</span>{" "}
                 <span className="text-[#757575]">{details.phone}</span>
               </p>
               <p className="flex items-center gap-2">
                 {" "}
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="18px"
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18px"
                     height="18"
-                    fill="gray" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></span>
+                    fill="gray"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                  </svg>
+                </span>
                 <span className="font-semibold">Email:</span>
                 <span className="text-[#757575]">{details.email}</span>
               </p>
@@ -124,26 +164,26 @@ const JobDetails = () => {
               <p className="flex gap-2">
                 {" "}
                 <span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18px"
-                  height="18"
-                  fill="gray"
-                  viewBox="0 0 384 512"
-                ><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18px"
+                    height="18"
+                    fill="gray"
+                    viewBox="0 0 384 512"
+                  >
+                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                  </svg>
                 </span>
-                <span className="font-semibold">Address:</span> {" "}
+                <span className="font-semibold">Address:</span>{" "}
                 <span className="text-[#757575]">{details.address}</span>
-               
-               
               </p>
             </div>
           </div>
 
           <div className="my-8">
-            <button className="btn-primary w-full" onClick={handleApplyNow}>Apply Now</button>
+            <button className="btn-primary w-full" onClick={handleApplyNow}>
+              Apply Now
+            </button>
           </div>
         </div>
       </div>
