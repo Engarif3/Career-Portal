3194cb1283bf6ced10bf39f6667f52e93be06fcc
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-04-12 15:20:59 +0200

    main.jx file updated and live link updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // components import lists
-import Layout from './components/Layout';
-import Home from './components/Home';
-import AppliedJobs from './components/AppliedJobs';
-import Blog from './components/Blog';
-import JobDetails from './components/JobDetails';
-import Assignment from './components/Assignment';
-import ErrorPage from './components/ErrorPage';
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import AppliedJobs from "./components/AppliedJobs";
+import Blog from "./components/Blog";
+import JobDetails from "./components/JobDetails";
+import Assignment from "./components/Assignment";
+import ErrorPage from "./components/ErrorPage";
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: "/",
     element: <Layout></Layout>,
-    errorElement:<ErrorPage></ErrorPage>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
-        path:'/',
+        path: "/",
         element: <Home></Home>,
-        loader:()=>fetch('/job_data.json')
+        loader: () => fetch("/job_data.json"),
       },
       {
-        path:'/applied_jobs',
-        element: <AppliedJobs></AppliedJobs>
+        path: "/applied_jobs",
+        element: <AppliedJobs></AppliedJobs>,
       },
       {
-        path:'/statistics',
-        element: <Assignment></Assignment>
+        path: "/statistics",
+        element: <Assignment></Assignment>,
       },
       {
-        path:'/blog',
-        element: <Blog></Blog>
+        path: "/blog",
+        element: <Blog></Blog>,
       },
       {
-        path:'/jobCard/:jobCardId',
+        path: "/jobCard/:jobCardId",
         element: <JobDetails></JobDetails>,
-        loader:()=>fetch('/job_data.json')
+        loader: () => fetch("/job_data.json"),
       },
       {
-        path:'/details',
-        element: <JobDetails></JobDetails>
-        
+        path: "/details",
+        element: <JobDetails></JobDetails>,
       },
+    ],
+  },
+]);
 
-    ]
-  }
-])
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
 
-
-// FeaturedJobs = friends
-// JobCard = friend
-// JobDetails = FriendDetails
